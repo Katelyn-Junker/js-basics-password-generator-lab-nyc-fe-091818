9ebbc88c615e597b0be03b216e58480c9f83bb1c
Katelyn Junker <kjunker@villanova.edu>
2018-11-02 00:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 window.addEventListener("load", () => {
   //start your code here
+  
+  // 1.figure out how long our password should be
+  
+  // 2.make a password from the ground up 
+  
+  // 3.we need to meet requirements 
+  
 });
