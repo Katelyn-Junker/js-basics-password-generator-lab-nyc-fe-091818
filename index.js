233595fcd358ea348b608944ad0985abcebe1b05
Katelyn Junker <kjunker@villanova.edu>
2018-11-02 00:21:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ window.addEventListener("load", () => {
   //start your code here
   
   // 1.figure out how long our password should be
+  document.addEventListener('submit', (event)=>{
+    // dont go to a new page or dont refresh
+    event.preventDefault()
+    console.log(event)
+  })
   
   // 2.make a password from the ground up 
   
